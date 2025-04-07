5a822978bf727928ad6f4e529d7591b176ef58f3
RenanFelipeh <renanfelipee457@gmail.com>
2025-04-06 22:01:57 -0300

    novo commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function adicionarCaracter(caracter){
+    const valorInput = document.querySelector('.display').value
+
+    document.querySelector('.display').value = valorInput + caracter
+    
+}
+
+function limparTela(){
+    document.querySelector('.display').value = ''
+}
+
+function calcular(){
+    const valorInput = document.querySelector('.display').value
+
+    document.querySelector('.display').value = eval(valorInput)
+}
